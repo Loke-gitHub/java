229ac49e6e5095710fe6c0ffae9522069e3d9f22
loke <1009483976@qq.com>
2020-06-18 18:19:21 +0800

    564

--- cdbuild.sh
+++ cdbuild.sh
@@ -5,7 +5,7 @@ docker-compose down
 docker-compose up -d
 docker image rm s2g3:latest
 docker commit soft2group3 s2g3
-sudo docker login --username=北城17云计算李沅罡 registry.cn-shanghai.aliyuncs.com
+sudo docker login --username=北城17云计算乔阳 registry.cn-shanghai.aliyuncs.com
 expect "password:"
 send "密码"   #输入密码--运行需删除注释
 sudo docker tag s2g3 registry.cn-shanghai.aliyuncs.com/lyg_leran_docker/lyg_cd:latest
